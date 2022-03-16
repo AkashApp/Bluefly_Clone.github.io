c50aae25e597c87d3466bbc1063b759e4a3b2db7
Akash Borate <27akashborate@gmail.com>
2022-03-16 21:49:25 +0530

    cors

--- Back_End/app.js
+++ Back_End/app.js
@@ -1,10 +1,11 @@
 const express= require("express");
 const Connect= require("./Configs/DbConnect.js");
 const productContro= require("./Controllers/productController");
-
+const cors= require("cors");
 const app= express();
 
 app.use(express.json());
+app.use(cors());
 app.use("/product", productContro);
 
 app.listen(7492, async(req,res)=>{
